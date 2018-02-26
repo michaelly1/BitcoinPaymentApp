18d07278621f9d7bc933850bd24a44aec2ed0c73
Rene <Rene>
2018-02-26 15:34:44 -0800

    base of walletform

--- src/main/java/bitcoinapp/WalletForm.java
+++ src/main/java/bitcoinapp/WalletForm.java
@@ -4,6 +4,18 @@ package bitcoinapp;
 Screen after logging into the app, should show wallet, addresses, balance, etc...
  */
 
-public class WalletForm {
+import javax.swing.*;
 
+public class WalletForm extends JFrame{
+
+    private JPanel panel1;
+
+    public WalletForm()
+    {
+        this.setTitle("bitcoin");
+        this.setContentPane(panel1);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+    }
 }
